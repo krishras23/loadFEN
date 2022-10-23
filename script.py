d7aae9e14660ddda098d71b34cbc22fef62d8640
Krish Rastogi <70422720+krishras23@users.noreply.github.com>
2022-10-23 00:40:15 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+PATH = "/Users/Soccerboy_Krish/Documents/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://lichess.org/analysis")
+
+fen_box = driver.find_element_by_xpath("/html/body/div[1]/main/div[4]/div/div[1]/input")
+for i in range(57):
+    fen_box.send_keys(Keys.BACK_SPACE)
+
+fen_box.send_keys("rnbq1bnr/ppppp1pp/4kp2/8/8/4KP2/PPPPP1PP/RNBQ1BNR w - - 4 4")
+fen_box.send_keys(Keys.RETURN)
