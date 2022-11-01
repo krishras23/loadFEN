743e54b1abcb80a5b61fec3c987b397e42da8661
Krish Rastogi <70422720+krishras23@users.noreply.github.com>
2022-11-01 06:23:26 -0700

    made func

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from tkinter.tix import FileEntry
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -7,9 +8,14 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://lichess.org/analysis")
 
-fen_box = driver.find_element_by_xpath("/html/body/div[1]/main/div[4]/div/div[1]/input")
-for i in range(57):
-    fen_box.send_keys(Keys.BACK_SPACE)
 
-fen_box.send_keys("rnbq1bnr/ppppp1pp/4kp2/8/8/4KP2/PPPPP1PP/RNBQ1BNR w - - 4 4")
-fen_box.send_keys(Keys.RETURN)
+def calc(FEN):
+    fen_box = driver.find_element_by_xpath("/html/body/div[1]/main/div[4]/div/div[1]/input")
+    for i in range(57):
+        fen_box.send_keys(Keys.BACK_SPACE)
+
+    fen_box.send_keys(FEN)
+    fen_box.send_keys(Keys.RETURN)
+
+
+calc("rnbq1bnr/ppppp1pp/4kp2/8/8/4KP2/PPPPP1PP/RNBQ1BNR w - - 4 4")
